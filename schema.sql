3e64f4e1c371f5f865e9a434ee49531e7189974c
Jul-9579 <raclotj@gmail.com>
2025-06-04 10:51:54 +0200

    new pipeline project

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE daily_stock_prices (
+    symbol TEXT NOT NULL,
+    date TEXT NOT NULL,
+    open REAL,
+    high REAL,
+    low REAL,
+    close REAL,
+    volume INTEGER,
+    PRIMARY KEY (symbol, date)
+);
